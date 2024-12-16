ebfd576966a51b0164fc45d4f2f19abec5d59210
Taha <fadilitaha05@gmail.com>
2024-12-16 23:31:48 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,10 @@ import './index.css'
 import { HUD } from './HUD'
 import { Landing } from './Landing'
 import { useStore } from './components/store'
+import UserInterface from './UI'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <HUD />
-    <Landing />
+    <UserInterface />
   </React.StrictMode>,
 )
